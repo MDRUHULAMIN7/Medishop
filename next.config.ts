56195f3ca31dad8c4360f566050c7c2c4eb9bbbc
mdruhulamin7 <ruhulthisis@gmail.com>
2026-08-17 19:10:19 +0600

    fix: add Cloudinary remotePatterns pathname and protocol support in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,40 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '**.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
